1c11060432dcbd49097733ed0233840edc6ae31c
Tyler Knight <TylerKnight2197@gmail.com>
2021-11-15 12:29:14 -0500

    W4_D1

---
+++ W4_D1/script.js
@@ -0,0 +1,12 @@
+//Write a function that accepts an integer and outputs the binary version of that integer
+
+
+//https://www.rapidtables.com/convert/number/decimal-to-binary.html
+
+const dec2binary = (num)=>{
+
+}
+
+
+
+dec2binary(58)//111010
