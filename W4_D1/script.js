fc95ad94103a82e38c9ce1b47f6c8bfc8d0cc7ab
Tyler Knight <TylerKnight2197@gmail.com>
2021-11-16 11:38:06 -0500

    W4_D1

--- W4_D1/script.js
+++ W4_D1/script.js
@@ -4,9 +4,24 @@
 //https://www.rapidtables.com/convert/number/decimal-to-binary.html
 
 const dec2binary = (num)=>{
-
+    let conversion=""
+    let tempNum = num
+    while(tempNum!=0){
+        conversion = tempNum % 2 + conversion
+        console.log(conversion)
+        tempNum=tempNum/2
+        tempNum=Math.floor(tempNum)
+        console.log(tempNum)
+        if(tempNum==1){
+            conversion=1+conversion
+            tempNum=0
+        } else if(tempNum==0) {
+            conversion=0+conversion
+        }
+    }
+    return conversion
 }
 
 
 
-dec2binary(58)//111010
+console.log(dec2binary(58))//111010
